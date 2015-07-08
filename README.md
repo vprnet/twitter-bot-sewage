a07f50d8fb5ce7bfb15b147188cd20ab2f3e8548
SARA SIMON <smbsimon@gmail.com>
2015-07-08 10:06:56 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#VPR's Sewage Twitter Bot - WIP
+
+This bot scrapes Vermont's Water Waste Inventory site and tweets when there's a sewage overflow or incident reported.
+
+##Questions?
+
+Get in touch. [@sarambsimon](http://twitter.com/sarambsimon).
---
+++ scrape.py
@@ -0,0 +1,22 @@
+import csv
+import requests
+from BeautifulSoup import BeautifulSoup
+
+url = 'https://anrweb.vt.gov/DEC/WWInventory/SewageOverflows.aspx'
+response = requests.get(url)
+html = response.content
+soup = BeautifulSoup(html)
+table = soup.find('table', attrs={'class': 'dataList'})
+
+list_of_rows = []
+for row in table.findAll('tr')[1:]:
+    list_of_cells = []
+    for cell in row.findAll('td'):
+        text = cell.text.replace('&nbsp;', '')
+        list_of_cells.append(text)
+    list_of_rows.append(list_of_cells)
+
+outfile = open("./sewage.csv", "wb")
+writer = csv.writer(outfile)
+writer.writerow(["Index", "Start Date", "End Date", "Start/End Times", "Municipality", "Location", "Waterbody", "Description of Incident", "Estimated Volume (gallons)", "Wastewater Treatment Facility", "Contact Person"])
+writer.writerows(list_of_rows)
