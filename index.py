9e8db9c06ff07f71b33e9d14230a977aeeaae792
SARA SIMON <smbsimon@gmail.com>
2015-07-15 16:26:17 -0400

    Restructured tweet to match restructured water waste inventory site

--- index.py
+++ index.py
@@ -8,8 +8,8 @@ def new_tweet():
         for row in reader:
             all_incidents.append(row)
 
-        if "gallons" not in all_incidents[1][8]:
-            tweet = all_incidents[1][4] + " - " + all_incidents[1][5] + " - " + all_incidents[1][7] + " (" + all_incidents[1][8] + " gallons estimated) "
+        if "gallons" not in all_incidents[1][7]:
+            tweet = all_incidents[1][4] + " - " + all_incidents[1][6] + " - " + " (" + all_incidents[1][7] + " gallons estimated) "
         else:
-            tweet = all_incidents[1][4] + " - " + all_incidents[1][5] + " - " + all_incidents[1][7] + " (" + all_incidents[1][8] + " estimated)"
+            tweet = all_incidents[1][4] + " - " + all_incidents[1][6] + " - " + " (" + all_incidents[1][7] + " estimated)"
         return tweet
