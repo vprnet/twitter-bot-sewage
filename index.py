dd622231784e8276acb5b39e4691f6b888c11df6
SARA SIMON <smbsimon@gmail.com>
2015-07-22 14:19:55 -0400

    Now that the site splits spills into two different tables, I needed to come up with a way to combine both tables and pull the most recent spill. This is a super sloppy way of going about it and refactoring is to come, I promise.

--- index.py
+++ index.py
@@ -8,8 +8,8 @@ def new_tweet():
         for row in reader:
             all_incidents.append(row)
 
-        if "gallons" not in all_incidents[1][7]:
-            tweet = all_incidents[1][4] + " - " + all_incidents[1][6] + " - " + " (" + all_incidents[1][7] + " gallons estimated) "
+        if "gallons" not in all_incidents[-1][7]:
+            tweet = all_incidents[-1][4] + " - " + all_incidents[-1][6] + " - " + " (" + all_incidents[-1][7] + " gallons estimated) "
         else:
-            tweet = all_incidents[1][4] + " - " + all_incidents[1][6] + " - " + " (" + all_incidents[1][7] + " estimated)"
+            tweet = all_incidents[-1][4] + " - " + all_incidents[-1][6] + " - " + " (" + all_incidents[-1][7] + " estimated)"
         return tweet
--- scrape.py
+++ scrape.py
@@ -6,17 +6,35 @@ url = 'https://anrweb.vt.gov/DEC/WWInventory/SewageOverflows.aspx'
 response = requests.get(url)
 html = response.content
 soup = BeautifulSoup(html)
-table = soup.find('table', attrs={'class': 'dataList'})
 
-list_of_rows = []
-for row in table.findAll('tr')[1:]:
+table1 = soup.find('table', attrs={'class': 'dataList', 'id': 'body_GridViewSewageOverflowsAuthorized'})
+table2 = soup.find('table', attrs={'class': 'dataList', 'id': 'body_GridViewSewageOverflowsOther'})
+
+list_of_rows1 = []
+list_of_rows2 = []
+final_list = []
+
+for row in table1.findAll('tr'):
     list_of_cells = []
     for cell in row.findAll('td'):
         text = cell.text.replace('&nbsp;', '')
         list_of_cells.append(text)
-    list_of_rows.append(list_of_cells)
+    list_of_rows1.append(list_of_cells)
+
+for row in table2.findAll('tr'):
+    list_of_cells = []
+    for cell in row.findAll('td'):
+        text = cell.text.replace('&nbsp;', '')
+        list_of_cells.append(text)
+    list_of_rows2.append(list_of_cells)
+
+final_list = list_of_rows1 + list_of_rows2
+
+while [] in final_list:
+    final_list.remove([])
+    final_list.sort()
 
 outfile = open("./sewage.csv", "wb")
 writer = csv.writer(outfile)
 writer.writerow(["Index", "Start Date", "End Date", "Start/End Times", "Municipality", "Location", "Waterbody", "Description of Incident", "Estimated Volume (gallons)", "Wastewater Treatment Facility", "Contact Person"])
-list = writer.writerows(list_of_rows)
+list = writer.writerows(final_list)
