fcd3fe2f88ebb1018327dd73673e87275b17f62d
SARA SIMON <smbsimon@gmail.com>
2015-07-24 13:04:05 -0400

    Restructure tweet logic to check if the tweet's been tweeted before.

--- index.py
+++ index.py
@@ -1,19 +1,20 @@
 import csv
-from datetime import date
 
 def new_tweet():
     with open('./sewage.csv', 'r') as f:
         reader = csv.reader(f)
         all_incidents = []
-        today = date.today()
+        tweet_history = []
 
         for row in reader:
             all_incidents.append(row)
 
-        if all_incidents[-1][2] == today:
+        if "gallons" not in all_incidents[-1][7]:
+            tweet = all_incidents[-1][4] + " - " + all_incidents[-1][6] + " - " + "(" + all_incidents[-1][7] + " gallons estimated) "
+        else:
+            tweet = all_incidents[-1][4] + " - " + all_incidents[-1][6] + " - " + "(" + all_incidents[-1][7] + " estimated)"
 
-            if "gallons" not in all_incidents[-1][7]:
-                tweet = all_incidents[-1][4] + " - " + all_incidents[-1][6] + " - " + " (" + all_incidents[-1][7] + " gallons estimated) "
-            else:
-                tweet = all_incidents[-1][4] + " - " + all_incidents[-1][6] + " - " + " (" + all_incidents[-1][7] + " estimated)"
+        if tweet not in tweet_history:
+
+            tweet_history.append(tweet)
             return tweet
